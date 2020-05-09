b6702261c8392e95884eb1f2d4b14b439c058b7e
andrewta999 <andrewta365@gmail.com>
2020-05-09 17:15:20 -0400

    added Signup Component

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Home from './shared/Home';
 import UserList from './userComponent/UserList';
+import Signup from './userComponent/Signup';
 
 const Router = () => {
     return(
@@ -9,6 +10,7 @@ const Router = () => {
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/users" component={UserList}/>
+                <Route path="/signup" component={Signup}/>
             </Switch>
         </div>
     );
